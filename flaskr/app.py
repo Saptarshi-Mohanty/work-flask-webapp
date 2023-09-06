ffa3df0869cb6f468ebab17e7f317ed7141a7b5f
saptarshi <saptarshi@assemblyline.store>
2023-09-06 11:42:16 +0530

    basic forms created

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,12 +1,19 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
+from formmodels import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
+SECRETS_URI = '3b301329dd4aec7ff92ca50e8e328f52'
+
 @app.route('/')
+def home():
+    return render_template('index.html')
+
 @app.route("/login")
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    return render_template('login.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
